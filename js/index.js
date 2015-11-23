5ae8806b01f3b1b87b913978eef842f74c7d2471
16thompsonjc1 <16thompsonjc1@stconline.edu.hk>
2015-11-23 08:56:27 +0800

    All Files As Of 22-11-2015

---
+++ js/index.js
@@ -0,0 +1,18 @@
+if (annyang) {
+	
+var commands = {
+    'Hello': function() {
+		alert("Ah, Hello There..."); },
+    'Who Are You': function() {
+		alert("Who Am I? None Of Your Business!"); },
+    'Do You Have A Dream': function() {
+		alert("Machines Don't Dream") },
+	'The Heck': function() {
+		alert("Da Hek XD!?") }
+};
+  
+annyang.debug(true);
+	annyang.addCommands(commands);
+ 
+annyang.start();
+}
