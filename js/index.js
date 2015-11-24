5becc1507c6bb19faeb33d350d1bf2f032ce0e3e
16thompsonjc1 <16thompsonjc1@stconline.edu.hk>
2015-11-24 10:40:23 +0800

    Change To JS So That I Can

--- js/index.js
+++ js/index.js
@@ -6,9 +6,12 @@ var commands = {
     'Who Are You': function() {
 		alert("Who Am I? None Of Your Business!"); },
     'Do You Have A Dream': function() {
-		alert("Machines Don't Dream") },
+		alert("Machines Don't Dream"); },
 	'The Heck': function() {
-		alert("Da Hek XD!?") }
+		alert("Da Hek XD!?"); },
+	'Continue': function () {
+		window.open('http://16thompsonjc1.github.io/Test-Site-16thompsonjc1/Selection%20Screen.html')
+	}
 };
   
 annyang.debug(true);
